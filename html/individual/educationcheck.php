c8f4f5d0aa61877257d31fd3626743c3a62afd51
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-08-23 14:57:26 +0300

    Update

--- html/individual/educationcheck.php
+++ html/individual/educationcheck.php
@@ -54,7 +54,7 @@ $errorcode = '';
 $certificate_photo_url = '';
 
 if (isset($_POST['test'])) {
-	echo $_POST["test"] === "checkindb" ? "true" : "false";
+	echo $_POST["MM_insert"] === "checkindb" ? "true" : "false";
 }
 
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] === "checkindb")) {
@@ -987,7 +987,7 @@ if ((isset($_GET['search_id_approve'])) && ($_GET['search_id_approve'] != "")) {
 																		<div class="modal-body padding">
 																			<form method="POST" enctype="multipart/form-data" action="<?php echo $editFormAction; ?>" class="form-horizontal">
 																				<input type="hidden" name="test" value="checkindb">
-																				<!-- <input type="hidden" name="MM_insert" value="checkindb"> -->
+																				<input type="hidden" name="MM_insert" value="checkindb">
 																				<input type="hidden" id="request_id" name="request_id" value="<?php echo $colname_getrequestid; ?>" />
 																				<input type="hidden" id="moduleid" name="moduleid" value="<?php echo $colname_getmoduleid; ?>" />
 																				<input type="hidden" id="search_id" name="search_id" value="<?php echo $search_ref; ?>" />
