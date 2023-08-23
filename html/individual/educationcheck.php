68a1143e7a062029c18c3736b50a9e88731227e6
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-08-23 16:49:07 +0300

    Update

--- html/individual/educationcheck.php
+++ html/individual/educationcheck.php
@@ -1778,7 +1778,16 @@ if ((isset($_GET['search_id_approve'])) && ($_GET['search_id_approve'] != "")) {
 	<!-- inline scripts related to this page -->
 	<script type="text/javascript">
 		ClassicEditor
-            .create( document.querySelector('#editor') )
+            .create( document.querySelector('#editor'), {
+								toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+								heading: {
+										options: [
+												{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+												{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+												{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+										]
+								}
+						})
             .catch(error => {
                 console.error( error );
             });
