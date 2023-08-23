609ed256200670b7697389634ee978b541d74a11
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-08-23 16:23:51 +0300

    Update

--- html/individual/educationcheck.php
+++ html/individual/educationcheck.php
@@ -1775,12 +1775,6 @@ if ((isset($_GET['search_id_approve'])) && ($_GET['search_id_approve'] != "")) {
 
 	<!-- inline scripts related to this page -->
 	<script type="text/javascript">
-		let form = document.getElementById('updateform')
-		console.log(form)
-		form.addEventListener('submit', function(e) {
-			e.preventDefault()
-			console.log('submitted')
-		})
 		jQuery(document).ready(function($) {
 			/** ******************************
 			 * Simple WYSIWYG
