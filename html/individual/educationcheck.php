a0b38bef2851a2b20a6fd1a59506f6a9ee580c41
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-08-23 14:08:52 +0300

    Update

--- html/individual/educationcheck.php
+++ html/individual/educationcheck.php
@@ -976,7 +976,7 @@ if ((isset($_GET['search_id_approve'])) && ($_GET['search_id_approve'] != "")) {
 
 																		<div class="modal-body padding">
 																			<form method="POST" enctype="multipart/form-data" action="<?php echo $editFormAction; ?>" class="form-horizontal">
-																				<input type="hidden" name="MM_insert" value="checkindb">
+																				<input type="text" name="MM_insert" value="checkindb">
 																				<input type="hidden" id="request_id" name="request_id" value="<?php echo $colname_getrequestid; ?>" />
 																				<input type="hidden" id="moduleid" name="moduleid" value="<?php echo $colname_getmoduleid; ?>" />
 																				<input type="hidden" id="search_id" name="search_id" value="<?php echo $search_ref; ?>" />
