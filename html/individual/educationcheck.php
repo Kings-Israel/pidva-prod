543b83fa506b288a40f2b15bd3b605f534295492
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-08-23 15:54:06 +0300

    Update

--- html/individual/educationcheck.php
+++ html/individual/educationcheck.php
@@ -52,11 +52,11 @@ if (isset($_GET['moduleid'])) {
 $errorcode = '';
 $certificate_photo_url = '';
 
-if (isset($_POST['test'])) {
-	echo $_POST['test'];
-}
+// if (isset($_POST['test'])) {
+// 	echo $_POST['test'];
+// }
 
-if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] === "checkindb")) {
+if ((isset($_POST['test'])) && ($_POST['test'] === 'checkindb')) {
 	if (is_uploaded_file($_FILES['certificate_photo']['tmp_name'])) {
 		date_default_timezone_set('Africa/Nairobi');
 		$date_insert = date('dmYhis');
